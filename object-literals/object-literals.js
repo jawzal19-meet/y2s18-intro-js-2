9954ca7519fb687bc7625c1fa89ab91a07e53d89
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:11:28 +0300

    added

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,12 @@
 // Create your 'me' object literal here!
+var me ={
+	firstName:"jawzal",
+	lastName:"sheikh khalil",
+	favoriteFoods:["sushi, pizza"],
+	age:"16",
+	favoriteFoodsimg:"https:halfoff.adspayusa.com/wp-content/uploads/2018/03/sushi_and_sashimi_for_two.0.jpg",
+	
+}
+
+me =JSON.stringify(me)
+document.write(me)
