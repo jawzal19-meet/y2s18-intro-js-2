0d849df4454c4c8740880c904be7f0f9a57df0b2
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 18:31:32 +0300

    ;)

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,6 +1,11 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ['./imgs/dog.jpg','secondimg.jpeg','thirdimg.jpeg','fourthimg.jpeg','fifthimg.jpeg'];
 var currentIndex = 0;
+
+
+//function chnageBackground()
+//document.body.style.backgroundcolor=color
+
 
 function showNextPicture() {
   currentIndex++; // increment current picture
@@ -10,4 +15,10 @@ function showNextPicture() {
   }
 
   // YOUR TASK: Finish this function!
+}
+
+function promptcolor(){
+	var color=prompt("choose a color");
+	var bodycolor =document.getElementsByTagName('body');
+	bodycolor[0].style.backgroundColor=color;
 }
